84622ee50adfb826b0e0071b98b1db0df5ee4505
wangwenxuanshidashuaige <1260571771@qq.com>
2021-07-20 23:06:22 +0800

    。

---
+++ 04/code/crud-express/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+
+const express = require('express');
+
+const app = express();
+
+
+
+app.get('/', function(req, res){
+
+})
+
+
+app.listen(3000, function(){
+    console.log('crud-express is running')
+})
